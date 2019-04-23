a6f2ed8572dc372104c4a3bb2143941bf9f35a9f
William Dry <wpbdry@gmail.com>
2019-04-23 22:09:48 +0200

    change search function to return dictionary

--- db.py
+++ db.py
@@ -1,20 +1,24 @@
 import psycopg2
+import psycopg2.extras
 
 import config
 
 
-def connect():
-    return psycopg2.connect(
+def execute_sql(query):
+    conn = psycopg2.connect(
         host=config.db_host,
         port=config.db_port,
         dbname=config.db_name,
         user=config.db_user,
         password=config.db_password)
-
-def execute_sql(query):
-    conn = connect()
-    cur = conn.cursor()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute(query)
-    function_return = cur.fetchall()
+    # The following is copied from
+    # https://stackoverflow.com/questions/16519385/output-pyodbc-cursor-results-as-python-dictionary
+    columns = [column[0] for column in cur.description]
+    function_return = []
+    for row in cur.fetchall():
+        function_return.append(dict(zip(columns, row)))
     cur.close()
+    conn.close()
     return function_return
