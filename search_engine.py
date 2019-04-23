85ed843970d8c3e1b92592e5c070585ae6b32fef
William Dry <wpbdry@gmail.com>
2019-04-23 20:58:53 +0200

    implement relatively intelligent search by all required parameters

--- search_engine.py
+++ search_engine.py
@@ -1,14 +1,28 @@
 import db
 
 
+def create_regex(search_term):
+    words = search_term.split(' ')
+    regex = ''
+    for item in range(len(words)):
+        regex += '(\m'
+        regex += words[item]
+        regex += '\M)'
+        if item < len(words) - 1:
+            regex += '|'
+    return regex
+
+
 def search_by_name(search_term):
     assert (search_term != ''), 'Search string should not be empty'
+    regex = create_regex(search_term)
     query = '''
         SELECT name, age, nationality, club, photo, overall, value
         FROM phiture_challenge.players
-        WHERE name LIKE '%{}%'
-    '''.format(search_term)
-    print(query)
+        WHERE name ~* '{0}'
+        OR nationality ~* '{0}'
+        OR club ~* '{0}';
+    '''.format(regex)
     matches = db.execute_sql(query)
     return matches
 
